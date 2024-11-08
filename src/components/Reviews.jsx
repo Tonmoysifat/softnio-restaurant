26e0a4925400c065b6ec54d7d688c0028d8ed348
Tonmuy Ahamed Sifat <tonmoysifatmd@gmail.com>
2024-11-08 18:34:36 +0600

    add title softnio restaurant

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -32,6 +32,7 @@ const Reviews = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
         autoplaySpeed: 3000,
         arrows: true,
         nextArrow: <NextArrow onClick={() => setPrevArrowActive(true)}/>,
